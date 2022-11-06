03be4889e9606641925b40aa872c2f3de3c5c6cf
xardas110 <xardas110@yahoo.no>
2022-11-06 11:56:28 +0100

    Scene graph

--- Engine/core/Components.h
+++ Engine/core/Components.h
@@ -2,3 +2,64 @@
 #include "Transform.h"
 
 using TransformComponent = Transform;
+
+struct TagComponent
+{
+	TagComponent(const std::string& tag) :tag(tag) {}
+
+	const std::string& GetTag() const
+	{
+		return tag;
+	}
+private:
+	std::string tag = "";
+};
+
+struct RelationComponent
+{
+	friend class Game;
+	friend class Entity;
+
+	~RelationComponent()
+	{
+		children.clear();
+	}
+
+	bool HasParent() const
+	{
+		return parent != entt::null;
+	}
+
+	bool HasChildren() const
+	{
+		return !children.empty();
+	}
+
+	entt::entity GetParent() const
+	{
+		return parent;
+	}
+
+	const std::vector<entt::entity>& GetChildren() const
+	{
+		return children;
+	}
+private:
+
+	void SetParent(entt::entity parent)
+	{
+		this->parent = parent;
+	}
+
+	void AddChild(entt::entity child)
+	{
+		for (size_t i = 0; i < children.size(); i++)
+		{
+			if (child == children[i]) return;
+		}
+		children.emplace_back(child);
+	}
+
+	entt::entity parent{ entt::null };
+	std::vector<entt::entity> children;
+};
