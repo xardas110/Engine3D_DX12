62047f1e6f865efe6e55bd1d7ab0e9b6c2a0539a
xardas110 <xardas110@yahoo.no>
2022-11-07 05:01:41 +0100

    -Added Sponza

--- Engine/core/AssimpLoader.h
+++ Engine/core/AssimpLoader.h
@@ -1,7 +1,98 @@
 #pragma once
+#include <Mesh.h>
 
-
-struct AssimpLoader
+namespace AssimpMaterialType
 {
+	enum Type
+	{
+		Ambient,
+		Albedo,
+		Diffuse = Albedo,
+		Specular,
+		Height,
+		Normal = Height,
+		Roughness,
+		Metallic,
+		Opacity,
+		Emissive,
+		Size
+	};
+}
+
+/*
+ Originally writted by me in Floof Engine
+ My initial plans was to work with the floof team, but I had to change due to compulsory conflics.
+*/
+class AssimpLoader
+{
+public:
 	AssimpLoader(const std::string& path);
+private:
+	
+	std::string path = "";
+
+	unsigned int aiFlags = aiProcess_CalcTangentSpace 
+		| aiProcess_Triangulate 
+		| aiProcess_GenUVCoords 
+		| aiProcess_FlipUVs;
+
+	struct AssimpTexture
+	{
+		std::string path = "";
+		bool IsValid() const
+		{
+			return !path.empty();
+		}
+	};
+
+	struct AssimpMaterial
+	{
+		bool HasTexture(AssimpMaterialType::Type type) const
+		{
+			return textures[type].IsValid();
+		}
+
+		AssimpTexture GetTexture(AssimpMaterialType::Type type) const
+		{
+			return textures[type];
+		}
+
+		AssimpTexture textures[AssimpMaterialType::Size];
+	};
+
+	struct AssimpVertex
+	{
+		DirectX::XMFLOAT3 pos{0.f, 0.f, 0.f};
+		DirectX::XMFLOAT3 norm{ 0.f, 0.f, 0.f };
+		DirectX::XMFLOAT2 uv{ 0.f, 0.f};
+		DirectX::XMFLOAT3 tangent{ 0.f, 0.f, 0.f };
+		DirectX::XMFLOAT3 bitTangent{ 0.f, 0.f, 0.f };
+	};
+
+	struct AssimpMesh
+	{
+		AssimpMaterial material;
+		std::vector<AssimpVertex> vertices;
+		std::vector<uint32_t> indices;
+	};
+
+	struct AssimpCollisionMesh
+	{
+		AssimpMesh mesh;
+	} collisionMesh;
+
+	struct AssimpStaticMesh
+	{
+		std::vector<AssimpMesh> meshes;
+	} staticMesh;
+
+	void ProcessNode(aiNode* node, const aiScene* scene);
+	bool LoadMesh(aiMesh* mesh, const aiScene* scene);
+	void LoadModel(const std::string& path);
+
+public:
+	const AssimpStaticMesh& GetAssimpStaticMesh() const
+	{
+		return staticMesh;
+	}
 };
