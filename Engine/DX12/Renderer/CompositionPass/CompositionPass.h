0fb7374db4db3696fb6da85254dd9ea66a7178fb
xardas110 <xardas110@yahoo.no>
2022-11-18 15:30:44 +0100

    CompositionPass

--- Engine/DX12/Renderer/CompositionPass/CompositionPass.h
+++ Engine/DX12/Renderer/CompositionPass/CompositionPass.h
@@ -1,16 +1,15 @@
 #pragma once
-#pragma once
 #include <RenderTarget.h>
 #include <RootSignature.h>
 #include <CommandList.h>
-#include <StaticDescriptorHeap.h>
 
-namespace LightPassParam
+namespace CompositionPassParam
 {
 	enum
 	{
 		AccelerationStructure,
 		GBufferHeap,
+		LightMapHeap,
 		GlobalHeapData,
 		GlobalMatInfo,
 		GlobalMaterials,
@@ -19,24 +18,19 @@ namespace LightPassParam
 	};
 }
 
-struct LightPass
+struct CompositionPass
 {
 private:
 	friend class DeferredRenderer;
 	friend class Window;
 
-	LightPass(const int& width, const int& height);
+	CompositionPass(const int& width, const int& height);
 	void CreateRenderTarget(int width, int height);
 	void CreatePipeline();
 
 	void ClearRendetTarget(CommandList& commandlist, float clearColor[4]);
 
 	void OnResize(int width, int height);
-
-	//Returns handle to heap start
-	D3D12_GPU_DESCRIPTOR_HANDLE CreateSRVViews();
-
-	SRVHeapData m_SRVHeap;
 
 	RenderTarget renderTarget;
 	RootSignature rootSignature;
