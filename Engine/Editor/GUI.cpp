93d5747806bab1f07e6f76f663049c35771cf452
xardas110 <xardas110@yahoo.no>
2022-11-18 21:24:47 +0100

    More refactor

--- Engine/Editor/GUI.cpp
+++ Engine/Editor/GUI.cpp
@@ -10,10 +10,6 @@
 #include <Texture.h>
 #include <Window.h>
 
-// Include compiled shaders for ImGui.
-#include <ImGUI_VS.h>
-#include <ImGUI_PS.h>
-
 #include <imgui_impl_win32.h>
 #include <imgui_impl_dx12.h>
 
