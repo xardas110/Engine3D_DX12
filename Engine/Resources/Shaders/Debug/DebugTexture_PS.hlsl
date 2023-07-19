a9e0880ae70e31f670dab8e20df3096fd6e0295b
xardas110 <xardas110@yahoo.no>
2023-07-19 10:37:12 +0200

    -Refactoring debugtexture

--- Engine/Resources/Shaders/Debug/DebugTexture_PS.hlsl
+++ Engine/Resources/Shaders/Debug/DebugTexture_PS.hlsl
@@ -1,14 +1,21 @@
-Texture2D g_Texture : register(t0);
-SamplerState g_NearestRepeatSampler : register(s0);
+// Texture resource
+Texture2D       g_Texture               : register(t0);
+
+// Sampler state for nearest-neighbor sampling
+SamplerState    g_NearestRepeatSampler : register(s0);
 
 struct PixelShaderOutput
 {
     float4 ColorTexture : SV_TARGET0;
 };
 
+// Main Pixel Shader function
 PixelShaderOutput main(float2 TexCoord : TEXCOORD)
 {
     PixelShaderOutput OUT;
+    
+    // Sample the texture using the provided texture coordinates
     OUT.ColorTexture = g_Texture.Sample(g_NearestRepeatSampler, TexCoord);
+    
     return OUT;
 }
--- Engine/Resources/Shaders/Debug/DebugTexture_VS.hlsl
+++ Engine/Resources/Shaders/Debug/DebugTexture_VS.hlsl
@@ -7,7 +7,14 @@ struct VertexShaderOutput
 VertexShaderOutput main(uint VertexID : SV_VertexID)
 {
     VertexShaderOutput OUT;
+    
+    // Calculate texture coordinates based on VertexID.
+    // This approach creates full-screen quad coordinates from the SV_VertexID.
+    // This is a common technique for full-screen quad rendering without using a vertex buffer.
     OUT.TexCoord = float2(uint2(VertexID, VertexID << 1) & 2);
+    
+    // Convert the texcoords into clip-space positions for a full-screen quad.
+    // (-1,-1) is the bottom-left and (1,1) is the top-right in clip-space.
     OUT.Position = float4(lerp(float2(-1, 1), float2(1, -1), OUT.TexCoord), 0, 1);
 
     return OUT;
