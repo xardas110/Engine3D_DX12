811d3e8e255e6e7e0be46ae26bd321266c8711fc
xardas110 <xardas110@yahoo.no>
2023-07-19 11:12:17 +0200

    -Cleaning shaders

--- Engine/Resources/Shaders/Common/HDR.hlsl
+++ Engine/Resources/Shaders/Common/HDR.hlsl
@@ -6,7 +6,7 @@
 #include "../Common/MaterialAttributes.hlsl"
 
 // Linear Tonemapping
-// Just normalizing based on some maximum luminance
+// Normalize HDR values based on a maximum luminance
 float3 Linear(float3 HDR, float max)
 {
     float3 SDR = HDR;
@@ -18,21 +18,21 @@ float3 Linear(float3 HDR, float max)
     return SDR;
 }
 
-// Reinhard tone mapping.
+// Reinhard tone mapping
 // See: http://www.cs.utah.edu/~reinhard/cdrom/tonemap.pdf
 float3 Reinhard(float3 HDR, float k)
 {
     return HDR / (HDR + k);
 }
 
-// Reinhard^2
+// Reinhard^2 tone mapping
 // See: http://www.cs.utah.edu/~reinhard/cdrom/tonemap.pdf
 float3 ReinhardSqr(float3 HDR, float k)
 {
     return pow(Reinhard(HDR, k), 2);
 }
 
-// ACES Filmic
+// ACES Filmic tone mapping
 // See: https://www.slideshare.net/ozlael/hable-john-uncharted2-hdr-lighting/142
 float3 ACESFilmic(float3 x, float A, float B, float C, float D, float E, float F)
 {
@@ -53,31 +53,31 @@ float3 _UnchartedCurve(float3 color)
 
 float3 HdrToLinear_Uncharted(float3 color)
 {
-            // John Hable's Uncharted 2 filmic tone map (http://filmicgames.com/archives/75)
+    // John Hable's Uncharted 2 filmic tone map (http://filmicgames.com/archives/75)
     return saturate(_UnchartedCurve(color) / _UnchartedCurve(11.2).x);
 }
 
-float3 ApplyTonemap(in float3 hdr, in TonemapCB tonemapParamaters)
+float3 ApplyTonemap(in float3 hdr, in TonemapCB tonemapParameters)
 {
-    // Add exposure to HDR result.
-    hdr *= exp2(tonemapParamaters.Exposure);
+    // Add exposure to HDR result
+    hdr *= exp2(tonemapParameters.Exposure);
 
-    // Perform tonemapping on HDR image.
-    float3 SDR = (float3) 0;
-    switch (tonemapParamaters.TonemapMethod)
+    // Perform tonemapping on HDR image
+    float3 SDR = float3(0, 0, 0);
+    switch (tonemapParameters.TonemapMethod)
     {
         case TM_Linear:
-            SDR = Linear(hdr, tonemapParamaters.MaxLuminance);
+            SDR = Linear(hdr, tonemapParameters.MaxLuminance);
             break;
         case TM_Reinhard:
-            SDR = Reinhard(hdr, tonemapParamaters.K);
+            SDR = Reinhard(hdr, tonemapParameters.K);
             break;
         case TM_ReinhardSq:
-            SDR = ReinhardSqr(hdr, tonemapParamaters.K);
+            SDR = ReinhardSqr(hdr, tonemapParameters.K);
             break;
         case TM_ACESFilmic:
-            SDR = ACESFilmic(hdr, tonemapParamaters.A, tonemapParamaters.B, tonemapParamaters.C, tonemapParamaters.D, tonemapParamaters.E, tonemapParamaters.F) /
-              ACESFilmic(tonemapParamaters.LinearWhite, tonemapParamaters.A, tonemapParamaters.B, tonemapParamaters.C, tonemapParamaters.D, tonemapParamaters.E, tonemapParamaters.F);
+            SDR = ACESFilmic(hdr, tonemapParameters.A, tonemapParameters.B, tonemapParameters.C, tonemapParameters.D, tonemapParameters.E, tonemapParameters.F) /
+                  ACESFilmic(tonemapParameters.LinearWhite, tonemapParameters.A, tonemapParameters.B, tonemapParameters.C, tonemapParameters.D, tonemapParameters.E, tonemapParameters.F);
             break;
         case TM_Uncharted:
             SDR = HdrToLinear_Uncharted(hdr);
--- Engine/Resources/Shaders/Common/Math.hlsl
+++ Engine/Resources/Shaders/Common/Math.hlsl
@@ -1,18 +1,15 @@
 #ifndef MATH_H
 #define MATH_H
 
-// Source: https://gamedev.stackexchange.com/questions/28395/rotating-vector3-by-a-quaternion
+// Rotate a point by a quaternion
 float3 RotatePoint(float4 q, float3 v)
 {
     const float3 qAxis = float3(q.x, q.y, q.z);
     return 2.0f * dot(qAxis, v) * qAxis + (q.w * q.w - dot(qAxis, qAxis)) * v + 2.0f * q.w * cross(qAxis, v);
 }
 
-float3x3 ConstructTBN(
-    in float3x3 model, 
-    in float3 tangent, 
-    in float3 biTangent, 
-    in float3 localNormal)
+// Construct a TBN matrix from model, tangent, bitangent, and local normal
+float3x3 ConstructTBN(in float3x3 model, in float3 tangent, in float3 biTangent, in float3 localNormal)
 {
     float3 T = normalize(float3(mul(model, tangent)));
     float3 B = normalize(float3(mul(model, biTangent)));
@@ -21,15 +18,15 @@ float3x3 ConstructTBN(
     return transpose(float3x3(T, B, N));
 }
 
-//Tangent normal in texturecoords
+// Convert tangent space normal to world space normal using TBN matrix
 float3 GetWorldNormal(in float3 tangentNormal, in float3x3 TBN)
 {
-    float3 normal;
-    normal = tangentNormal * 2.f - 1.f;
+    float3 normal = tangentNormal * 2.f - 1.f;
     normal = normalize(mul(TBN, normal));
     return normal;
 }
 
+// Convert UV coordinates and depth to world space position
 float3 GetWorldPosFromDepth(in float2 uv, float depth, in float4x4 invViewProj)
 {
     float4 ndc = float4(uv * 2.0f - 1.0f, depth, 1.0f);
@@ -38,39 +35,41 @@ float3 GetWorldPosFromDepth(in float2 uv, float depth, in float4x4 invViewProj)
     return (wp / wp.w).xyz;
 }
 
+// Convert UV coordinates to clip space
 float2 UVToClip(float2 UV)
 {
     return float2(UV.x * 2 - 1, 1 - UV.y * 2);
 }
 
+// Convert clip space coordinates to UV coordinates
 float2 ClipToUV(float2 ClipPos)
 {
     return float2(ClipPos.x * 0.5 + 0.5, 0.5 - ClipPos.y * 0.5);
 }
 
-#define _Pi( x ) radians( 180.0 * x )
-
+// Pi function for a single float value
 float Pi(float x)
 {
-    return _Pi(x);
+    return radians(180.0 * x);
 }
 
+// Pi function for float2, float3, and float4 values
 float2 Pi(float2 x)
 {
-    return _Pi(x);
+    return radians(180.0 * x);
 }
 
 float3 Pi(float3 x)
 {
-    return _Pi(x);
+    return radians(180.0 * x);
 }
 
 float4 Pi(float4 x)
 {
-    return _Pi(x);
+    return radians(180.0 * x);
 }
 
-// Sqrt for values in range [0; 1]
+// Square root for values in the range [0, 1]
 float Sqrt01(float x)
 {
     return sqrt(saturate(x));
@@ -91,15 +90,12 @@ float4 Sqrt01(float4 x)
     return sqrt(saturate(x));
 }
 
-// These functions are used to compute power operations on values within the range [0; 1]. 
-
-// Pow01 for single float values
+// Power function for values in the range [0, 1]
 float Pow01(float x, float y)
 {
     return pow(saturate(x), y);
 }
 
-// Pow01 for float2 values
 float2 Pow01(float2 x, float y)
 {
     return pow(saturate(x), y);
@@ -110,7 +106,6 @@ float2 Pow01(float2 x, float2 y)
     return pow(saturate(x), y);
 }
 
-// Pow01 for float3 values
 float3 Pow01(float3 x, float y)
 {
     return pow(saturate(x), y);
@@ -121,7 +116,6 @@ float3 Pow01(float3 x, float3 y)
     return pow(saturate(x), y);
 }
 
-// Pow01 for float4 values
 float4 Pow01(float4 x, float y)
 {
     return pow(saturate(x), y);
@@ -132,6 +126,7 @@ float4 Pow01(float4 x, float4 y)
     return pow(saturate(x), y);
 }
 
+// Convert linear space color to sRGB space
 float3 LinearToSrgb(float3 color)
 {
     const float4 consts = float4(1.055, 0.41666, -0.055, 12.92);
@@ -140,11 +135,11 @@ float3 LinearToSrgb(float3 color)
     return lerp(consts.x * pow(color, consts.yyy) + consts.zzz, consts.w * color, color < 0.0031308);
 }
 
+// Convert sRGB space color to linear space
 float3 SrgbToLinear(float3 srgbColor)
 {
     const float4 consts = float4(1.0 / 12.92, 1.0 / 1.055, 0.055 / 1.055, 2.4);
     srgbColor = saturate(srgbColor);
     return lerp(srgbColor * consts.x, pow(srgbColor * consts.y + consts.zzz, consts.www), srgbColor > 0.04045);
 }
-
 #endif
