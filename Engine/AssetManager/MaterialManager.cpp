8170f06c3c1c0de0eea9e359a2b32cae7b544811
xardas110 <xardas110@yahoo.no>
2023-07-22 18:51:27 +0200

    -Refactor MaterialManager for threadsafety

--- Engine/AssetManager/MaterialManager.cpp
+++ Engine/AssetManager/MaterialManager.cpp
@@ -3,82 +3,8 @@
 #include <TextureManager.h>
 #include <Texture.h>
 
-std::wstring g_NoMaterial = L"No Material";
-MaterialColor g_NoUserMaterial; //Already initialized with values
-
-void PopulateGPUInfo(MaterialInfoGPU& gpuInfo, const TextureInstance& instance, UINT& gpuField)
-{
-	if (instance.IsValid())
-	{
-		gpuField = instance.GetTextureGPUHandle().value();
-	}
-}
-
-const MaterialInstance& MaterialManager::LoadMaterial(const std::wstring& name, const MaterialInfoCPU& textureIDs, const MaterialColor& materialColor)
-{
-	// Shared mutex for materialinstance map lookup
-	{
-		SHARED_LOCK(MaterialRegistryMap, materialRegistry.mapMutex);
-		if (materialRegistry.map.find(name) != materialRegistry.map.end())
-		{
-			return materialRegistry.map[name];
-		}
-	}
-
-	return CreateMaterial(name, textureIDs, materialColor);
-}
-
-const MaterialInstance& MaterialManager::CreateMaterial(const std::wstring& name, const MaterialInfoCPU& textureIDs, const MaterialColor& materialColor)
-{
-	MaterialID currentIndex = 0;
-
-	// Lock the registry mutexes for write operations.
-	{
-		SCOPED_UNIQUE_LOCK(
-			releasedMaterialIDsMutex, 
-			materialRegistry.cpuInfoMutex, 
-			materialRegistry.gpuInfoMutex, 
-			materialRegistry.materialColorsMutex);
-
-		if (!releasedMaterialIDs.empty())
-		{
-			currentIndex = releasedMaterialIDs.front();
-			releasedMaterialIDs.erase(releasedMaterialIDs.begin());
-		}
-		else
-		{
-			currentIndex = materialRegistry.cpuInfo.size(),
-			assert(currentIndex < MATERIAL_MANAGER_MAX_MATERIALS && "Material Manager exceeds maximum materials");
-
-			materialRegistry.cpuInfo.emplace_back(textureIDs);
-			materialRegistry.gpuInfo.emplace_back(MaterialInfoGPU());
-			materialRegistry.materialColors.emplace_back(materialColor);
-			materialRegistry.refCounts[currentIndex].store(0U);
-		}
-	}
-
-	assert(materialRegistry.cpuInfo.size() == materialRegistry.gpuInfo.size());
-	assert(materialRegistry.gpuInfo.size() == materialRegistry.materialColors.size());
-
-	// Lock GPUInfo mutex and populate GPUInfo for the current material ID.
-	{
-		UNIQUE_LOCK(MaterialRegistryGPUInfo, materialRegistry.gpuInfoMutex);
-		PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::albedo], materialRegistry.gpuInfo[currentIndex].albedo);
-		PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::normal], materialRegistry.gpuInfo[currentIndex].normal);
-		PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::ao], materialRegistry.gpuInfo[currentIndex].ao);
-		PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::emissive], materialRegistry.gpuInfo[currentIndex].emissive);
-		PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::roughness], materialRegistry.gpuInfo[currentIndex].roughness);
-		PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::specular], materialRegistry.gpuInfo[currentIndex].specular);
-		PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::metallic], materialRegistry.gpuInfo[currentIndex].metallic);
-		PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::lightmap], materialRegistry.gpuInfo[currentIndex].lightmap);
-		PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::opacity], materialRegistry.gpuInfo[currentIndex].opacity);
-		PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::height], materialRegistry.gpuInfo[currentIndex].height);
-	}
-
-	UNIQUE_LOCK(MaterialRegistryMap, materialRegistry.mapMutex);
-	materialRegistry.map[name] = MaterialInstance(currentIndex);
-	return materialRegistry.map[name];
-}
+const std::wstring MaterialManager::ms_NoMaterial = L"No Material";
+const MaterialColor MaterialManager::ms_DefaultMaterialColor = MaterialColor();
 
 bool MaterialManager::IsMaterialValid(const MaterialID materialID) const
 {
@@ -124,7 +50,7 @@ void MaterialManager::ReleaseMaterial(const MaterialID materialID)
 	if (!IsMaterialValid(materialID))
 		return;
 
-	LOG_INFO("Releaseing material %i", materialID);
+	LOG_INFO("Releasing material: %i", materialID);
 
 	materialRegistry.refCounts[materialID].store(0U);
 
@@ -172,67 +98,33 @@ std::optional<MaterialInstance> MaterialManager::GetMaterialInstance(const std::
 
 void MaterialManager::SetFlags(const MaterialInstance& materialInstance, const UINT flags)
 {
+	SCOPED_UNIQUE_LOCK(materialRegistry.cpuInfoMutex, materialRegistry.gpuInfoMutex);
 	materialRegistry.gpuInfo[materialInstance.materialID].flags = flags;
 	materialRegistry.cpuInfo[materialInstance.materialID].flags = flags;
 }
 
 void MaterialManager::AddFlags(const MaterialInstance& materialInstance, const UINT flags)
 {
+	SCOPED_UNIQUE_LOCK(materialRegistry.cpuInfoMutex, materialRegistry.gpuInfoMutex);
 	materialRegistry.gpuInfo[materialInstance.materialID].flags |= flags;
 	materialRegistry.cpuInfo[materialInstance.materialID].flags |= flags;
 }
 
 TextureInstance MaterialManager::GetTextureInstance(const MaterialInstance& materialInstance, MaterialType::Type type)
 {
-	auto& matInfo = materialRegistry.cpuInfo[materialInstance.materialID];
-	return matInfo.textures[type];
-}
-
-const std::wstring& MaterialManager::GetMaterialName(const MaterialInstance& materialInstance) const
-{
-	if (materialInstance.materialID == MATERIAL_INVALID)
-		return g_NoMaterial;
-
-	for (const auto& [name, id] : materialRegistry.map)
-	{
-		if (id.materialID == materialInstance.materialID)
-			return name;
-	}
-
-	return g_NoMaterial;
-}
-
-const MaterialColor& MaterialManager::GetMaterialColor(const MaterialInstance& materialInstance)
-{
-	if (materialInstance.materialID == MATERIAL_INVALID)
-	{
-		return g_NoUserMaterial;
-	}
-
-	return materialRegistry.materialColors[materialInstance.materialID];
+	SHARED_LOCK(MaterialRegistryCPUInfo, materialRegistry.cpuInfoMutex);
+	return materialRegistry.cpuInfo[materialInstance.materialID].textures[type];
 }
 
 UINT MaterialManager::GetCPUFlags(const MaterialInstance& materialInstance) const
 {
+	SHARED_LOCK(MaterialRegistryCPUInfo, materialRegistry.cpuInfoMutex);
 	return materialRegistry.cpuInfo[materialInstance.materialID].flags;
 }
 
 UINT MaterialManager::GetGPUFlags(const MaterialInstance& materialInstance) const
 {
+	SHARED_LOCK(MaterialRegistryGPUInfo, materialRegistry.gpuInfoMutex);
 	return materialRegistry.gpuInfo[materialInstance.materialID].flags;
 }
 
-const std::vector<MaterialInfoCPU>& MaterialManager::GetMaterialCPUInfoData() const
-{
-	return materialRegistry.cpuInfo;
-}
-
-const std::vector<MaterialInfoGPU>& MaterialManager::GetMaterialGPUInfoData() const
-{
-	return materialRegistry.gpuInfo;
-}
-
-const std::vector<MaterialColor>& MaterialManager::GetMaterialColorData() const
-{
-	return materialRegistry.materialColors;
-}
--- Engine/AssetManager/MaterialManager.h
+++ Engine/AssetManager/MaterialManager.h
@@ -3,6 +3,8 @@
 #include <Material.h>
 #include <map>
 #include <array>
+#include <type_traits>
+#include <assert.h>
 #include <AssetManagerDefines.h>
 
 class TextureData;
@@ -15,24 +17,45 @@ class MaterialManager
     friend class MaterialInstance;
 
 public:
-    const MaterialInstance& LoadMaterial(const std::wstring& name, const MaterialInfoCPU& textureIDs, const MaterialColor& materialColor);
+
+    template<bool ThreadSafe = ASSET_MANAGER_THREAD_SAFE>
+    auto LoadMaterial(const std::wstring& name, const MaterialInfoCPU& textureIDs, const MaterialColor& materialColor)
+        -> std::conditional_t<ThreadSafe, MaterialInstance, const MaterialInstance&>;
+
     std::optional<MaterialInstance> GetMaterialInstance(const std::wstring& name);
     void SetFlags(const MaterialInstance& materialInstance, const UINT flags);
     void AddFlags(const MaterialInstance& materialInstance, const UINT flags);
     bool IsMaterialValid(const MaterialInstance& materialInstance) const;
+
     TextureInstance GetTextureInstance(const MaterialInstance& materialInstance, MaterialType::Type type);
-    const std::wstring& GetMaterialName(const MaterialInstance& materialInstance) const;
-    const MaterialColor& GetMaterialColor(const MaterialInstance& materialInstance);
+
+    template <bool ThreadSafe = ASSET_MANAGER_THREAD_SAFE>
+    auto GetMaterialName(const MaterialInstance& materialInstance) const
+        ->std::conditional_t<ThreadSafe, std::wstring, const std::wstring&>;
+
+    template <bool ThreadSafe = ASSET_MANAGER_THREAD_SAFE>
+    auto GetMaterialColor(const MaterialInstance& materialInstance)
+        -> std::conditional_t<ThreadSafe, MaterialColor, const MaterialColor&>;
+
     UINT GetCPUFlags(const MaterialInstance& materialInstance) const;
     UINT GetGPUFlags(const MaterialInstance& materialInstance) const;
-    const std::vector<MaterialInfoCPU>& GetMaterialCPUInfoData() const;
-    const std::vector<MaterialInfoGPU>& GetMaterialGPUInfoData() const;
-    const std::vector<MaterialColor>& GetMaterialColorData() const;
+
+    template <bool ThreadSafe = ASSET_MANAGER_THREAD_SAFE>
+    auto GetMaterialCPUInfoData() const;
+
+    template <bool ThreadSafe = ASSET_MANAGER_THREAD_SAFE>
+    auto GetMaterialGPUInfoData() const;
+
+    template <bool ThreadSafe = ASSET_MANAGER_THREAD_SAFE>
+    auto GetMaterialColorData() const;
 
 private:
     bool IsMaterialValid(const MaterialID materialID) const;
 
-    const MaterialInstance& CreateMaterial(const std::wstring& name, const MaterialInfoCPU& textureIDs, const MaterialColor& materialColor);
+    template<bool ThreadSafe = ASSET_MANAGER_THREAD_SAFE>
+    auto CreateMaterial(const std::wstring& name, const MaterialInfoCPU& textureIDs, const MaterialColor& materialColor)
+        -> std::conditional_t<ThreadSafe, MaterialInstance, const MaterialInstance&>;
+
     void IncreaseRefCount(const MaterialID materialID);
     void DecreaseRefCount(const MaterialID materialID);
     void ReleaseMaterial(const MaterialID materialID);
@@ -45,8 +68,6 @@ private:
         std::array<std::atomic<RefCount>, MATERIAL_MANAGER_MAX_MATERIALS> refCounts;
         std::map<std::wstring, MaterialInstance> map;
 
-
-        // Mutexes for thread safety
         CREATE_MUTEX(map);
         CREATE_MUTEX(materialColors);
         CREATE_MUTEX(gpuInfo);
@@ -55,4 +76,179 @@ private:
 
     CREATE_MUTEX(releasedMaterialIDs);
     std::vector<MaterialID> releasedMaterialIDs;
+
+    const static std::wstring ms_NoMaterial;
+    const static MaterialColor ms_DefaultMaterialColor;
 };
+
+inline void PopulateGPUInfo(MaterialInfoGPU& gpuInfo, const TextureInstance& instance, UINT& gpuField)
+{
+    if (instance.IsValid())
+    {
+        gpuField = instance.GetTextureGPUHandle().value();
+    }
+}
+
+template<bool ThreadSafe>
+inline auto MaterialManager::LoadMaterial(
+    const std::wstring& name, 
+    const MaterialInfoCPU& textureIDs, 
+    const MaterialColor& materialColor)
+-> std::conditional_t<ThreadSafe, MaterialInstance, const MaterialInstance&>
+{
+    if constexpr (ThreadSafe)
+        static_assert(ASSET_MANAGER_THREAD_SAFE && 
+            "This function is not thread safe since ASSET_MANAGER_THREAD_SAFE == false");
+
+    // Shared mutex for materialinstance map lookup
+    {
+        SHARED_LOCK(MaterialRegistryMap, materialRegistry.mapMutex);
+        if (materialRegistry.map.find(name) != materialRegistry.map.end())
+        {
+            return materialRegistry.map[name];
+        }
+    }
+
+    return CreateMaterial(name, textureIDs, materialColor);
+}
+
+template<bool ThreadSafe>
+inline auto MaterialManager::CreateMaterial(
+    const std::wstring& name, 
+    const MaterialInfoCPU& textureIDs, 
+    const MaterialColor& materialColor)
+-> std::conditional_t<ThreadSafe, MaterialInstance, const MaterialInstance&>
+{
+
+    if constexpr (ThreadSafe)
+        static_assert(ASSET_MANAGER_THREAD_SAFE &&
+            "This function is not thread safe since ASSET_MANAGER_THREAD_SAFE == false");
+
+    MaterialID currentIndex = 0;
+
+    // Lock the registry mutexes for write operations.
+    {
+        SCOPED_UNIQUE_LOCK(
+            releasedMaterialIDsMutex,
+            materialRegistry.cpuInfoMutex,
+            materialRegistry.gpuInfoMutex,
+            materialRegistry.materialColorsMutex);
+
+        if (!releasedMaterialIDs.empty())
+        {
+            currentIndex = releasedMaterialIDs.front();
+            releasedMaterialIDs.erase(releasedMaterialIDs.begin());
+        }
+        else
+        {
+            currentIndex = materialRegistry.cpuInfo.size(),
+                assert(currentIndex < MATERIAL_MANAGER_MAX_MATERIALS && "Material Manager exceeds maximum materials");
+
+            materialRegistry.cpuInfo.emplace_back(textureIDs);
+            materialRegistry.gpuInfo.emplace_back(MaterialInfoGPU());
+            materialRegistry.materialColors.emplace_back(materialColor);
+            materialRegistry.refCounts[currentIndex].store(0U);
+        }
+    }
+
+    assert(materialRegistry.cpuInfo.size() == materialRegistry.gpuInfo.size());
+    assert(materialRegistry.gpuInfo.size() == materialRegistry.materialColors.size());
+
+    // Lock GPUInfo mutex and populate GPUInfo for the current material ID.
+    {
+        UNIQUE_LOCK(MaterialRegistryGPUInfo, materialRegistry.gpuInfoMutex);
+        PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::albedo], materialRegistry.gpuInfo[currentIndex].albedo);
+        PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::normal], materialRegistry.gpuInfo[currentIndex].normal);
+        PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::ao], materialRegistry.gpuInfo[currentIndex].ao);
+        PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::emissive], materialRegistry.gpuInfo[currentIndex].emissive);
+        PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::roughness], materialRegistry.gpuInfo[currentIndex].roughness);
+        PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::specular], materialRegistry.gpuInfo[currentIndex].specular);
+        PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::metallic], materialRegistry.gpuInfo[currentIndex].metallic);
+        PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::lightmap], materialRegistry.gpuInfo[currentIndex].lightmap);
+        PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::opacity], materialRegistry.gpuInfo[currentIndex].opacity);
+        PopulateGPUInfo(materialRegistry.gpuInfo[currentIndex], textureIDs.textures[MaterialType::height], materialRegistry.gpuInfo[currentIndex].height);
+    }
+
+    UNIQUE_LOCK(MaterialRegistryMap, materialRegistry.mapMutex);
+    materialRegistry.map[name] = MaterialInstance(currentIndex);
+    return materialRegistry.map[name];
+}
+
+template <bool ThreadSafe>
+auto MaterialManager::GetMaterialName(const MaterialInstance& materialInstance) const
+-> std::conditional_t<ThreadSafe, std::wstring, const std::wstring&>
+{
+    if constexpr (ThreadSafe)
+        static_assert(ASSET_MANAGER_THREAD_SAFE && 
+            "The function is not thread safe if ASSET_MANAGER_THREAD_SAFE == false");
+
+    if (!IsMaterialValid(materialInstance.materialID))
+        return MaterialManager::ms_NoMaterial;
+
+    SHARED_LOCK(MaterialRegistryMap, materialRegistry.mapMutex);
+    for (const auto& [name, id] : materialRegistry.map)
+    {
+        if (id.materialID == materialInstance.materialID)
+            return name;
+    }
+
+    return MaterialManager::ms_NoMaterial;
+}
+
+template <bool ThreadSafe>
+auto MaterialManager::GetMaterialColor(const MaterialInstance& materialInstance)
+-> std::conditional_t<ThreadSafe, MaterialColor, const MaterialColor&>
+{
+    if constexpr (ThreadSafe)
+        static_assert(ASSET_MANAGER_THREAD_SAFE && 
+            "The function is not thread safe if ASSET_MANAGER_THREAD_SAFE == false");
+
+    if (!IsMaterialValid(materialInstance.materialID))
+        return MaterialManager::ms_DefaultMaterialColor;
+
+    SHARED_LOCK(MaterialRegistryMaterialColors, materialRegistry.materialColorsMutex);
+    return materialRegistry.materialColors[materialInstance.materialID];
+}
+
+template <>
+inline auto MaterialManager::GetMaterialCPUInfoData<true>() const
+{
+    static_assert(ASSET_MANAGER_THREAD_SAFE && 
+        "The function is not thread safe if ASSET_MANAGER_THREAD_SAFE == false");
+    SHARED_LOCK(MaterialRegistryCPUInfo, materialRegistry.cpuInfoMutex);
+    return materialRegistry.cpuInfo;
+}
+
+template <>
+inline auto MaterialManager::GetMaterialCPUInfoData<false>() const
+{
+    return std::ref(materialRegistry.cpuInfo);
+}
+
+template <>
+inline auto MaterialManager::GetMaterialGPUInfoData<true>() const
+{
+    static_assert(ASSET_MANAGER_THREAD_SAFE && "The function is not thread safe if ASSET_MANAGER_THREAD_SAFE == false");
+    SHARED_LOCK(MaterialRegistryGPUInfo, materialRegistry.gpuInfoMutex);
+    return materialRegistry.gpuInfo;
+}
+
+template <>
+inline auto MaterialManager::GetMaterialGPUInfoData<false>() const
+{
+    return std::ref(materialRegistry.gpuInfo);
+}
+
+template <>
+inline auto MaterialManager::GetMaterialColorData<true>() const
+{
+    static_assert(ASSET_MANAGER_THREAD_SAFE && "The function is not thread safe if ASSET_MANAGER_THREAD_SAFE == false");
+    SHARED_LOCK(MaterialRegistryMaterialColors, materialRegistry.materialColorsMutex);
+    return materialRegistry.materialColors;
+}
+
+template <>
+inline auto MaterialManager::GetMaterialColorData<false>() const
+{
+    return std::ref(materialRegistry.materialColors);
+}
